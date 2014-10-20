a676d27ffca0d0588c45596690039575db53df84
Jörn Horstmann <git@jhorstmann.net>
2014-10-20 22:02:28 +0200

    Example for failing and ignored example scenarios

--- zuchini-junit/src/test/java/org/zuchini/junit/cukes/CukesSteps.java
+++ zuchini-junit/src/test/java/org/zuchini/junit/cukes/CukesSteps.java
@@ -1,19 +1,25 @@
 package org.zuchini.junit.cukes;
 
 import org.junit.Assert;
+import org.junit.internal.AssumptionViolatedException;
 import org.zuchini.annotations.Given;
 import org.zuchini.annotations.Then;
 
 public class CukesSteps {
     private int cukes;
 
-    @Given("^I have (\\d+) cukes in my belly$")
+    @Given("^I have (-?\\d+) cukes in my belly$")
     public void i_have_cukes_in_my_belly(int cukes) {
+        if (cukes < 0) {
+            throw new IllegalArgumentException("cukes can not be negative");
+        } else if(cukes == 0) {
+            throw new AssumptionViolatedException("cukes can not be 0");
+        }
         this.cukes = cukes;
 
     }
 
-    @Then("^there are (\\d+) cukes in my belly$")
+    @Then("^there are (-?\\d+) cukes in my belly$")
     public void there_are_cukes_in_my_belly(int cukes) {
         Assert.assertEquals(cukes, this.cukes);
     }
--- zuchini-junit/src/test/resources/features/cukes/cukes.feature
+++ zuchini-junit/src/test/resources/features/cukes/cukes.feature
@@ -1,5 +1,15 @@
 Feature: cukes
 
-Scenario: Some cukes
-Given I have 48 cukes in my belly
-Then there are 48 cukes in my belly
+  Scenario: Some cukes
+    Given I have 48 cukes in my belly
+    Then there are 48 cukes in my belly
+
+  Scenario Outline: More cukes
+    Given I have <count> cukes in my belly
+    Then there are <count> cukes in my belly
+  Examples:
+  | count |
+  | -1    |
+  | 0     |
+  | 1     |
+  | 2     |
