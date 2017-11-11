dbd0bcae120ce8f0739a8c68b9d6238714abc89b
Jörn Horstmann <git@jhorstmann.net>
2017-11-11 19:26:46 +0100

    Modify release script to automatically checkout release branch and to create a separate branch for next development iteration

--- release.sh
+++ release.sh
@@ -6,10 +6,14 @@ DEFAULT_VERSION=1.0-SNAPSHOT
 RELEASE_VERSION=${1?"Usage $0 release-version [next-version]"}
 NEXT_VERSION=${2-$DEFAULT_VERSION}
 
+git checkout -b release/$RELEASE_VERSION
+
 # release
 mvn scm:check-local-modification versions:set -DnewVersion=$RELEASE_VERSION scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="Release $RELEASE_VERSION"
 
 mvn -Prelease clean scm:tag deploy
+
+git checkout -b dev/$NEXT_VERSION
 
 # next development version
 mvn versions:set -DnewVersion=$NEXT_VERSION scm:add -Dincludes="**/pom.xml" scm:checkin -Dmessage="Develop $NEXT_VERSION"
