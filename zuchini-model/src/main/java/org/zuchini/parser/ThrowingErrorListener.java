665a7808b246aed443ec03a9473cbf767f30d7c0
Jörn Horstmann <git@jhorstmann.net>
2017-11-16 12:06:44 +0100

    Wrap the RecognitionException in case it is non-null

--- zuchini-model/src/main/java/org/zuchini/parser/ThrowingErrorListener.java
+++ zuchini-model/src/main/java/org/zuchini/parser/ThrowingErrorListener.java
@@ -12,6 +12,6 @@ class ThrowingErrorListener extends BaseErrorListener {
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
             throws ParseCancellationException {
-        throw new ParseCancellationException("line " + line + ":" + charPositionInLine + " " + msg);
+        throw new ParseCancellationException("line " + line + ":" + charPositionInLine + " " + msg, e);
     }
 }
